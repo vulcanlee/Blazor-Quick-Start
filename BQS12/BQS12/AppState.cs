f06cb0bdc8e395c4d8968d773cd4d1553dd4a41f
Vulcan <vulcan.lee@gmail.com>
2021-04-21 23:39:15 +0800

    BQS12|元件之間的委派與呼叫設計

---
+++ BQS12/BQS12/AppState.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BQS12
+{
+    public class AppState
+    {
+        public Action OnChange;
+        public Action<string> OnMessageChange;
+
+        public void UpdateAllView()
+        {
+            NotifyStateChanged();
+        }
+
+        public void RefreshMessageAllView(string message)
+        {
+            NotifyMessageStateChanged(message);
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+        private void NotifyMessageStateChanged(string message) => OnMessageChange?.Invoke(message);
+    }
+}
