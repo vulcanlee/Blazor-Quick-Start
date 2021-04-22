7dc66fb5b0f5b2eb95562d93a832010df401679b
Vulcan <vulcan.lee@gmail.com>
2021-04-22 20:52:55 +0800

    BQS15|元件傳遞資料 -外層變化，通知內層 (串聯參數+狀態容器方法)

---
+++ BQS15/BQS15/NotificationCenter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BQS15
+{
+    public class NotificationCenter
+    {
+        public Action<string> OnMessageChange;
+
+        public void RefreshMessageAllView(string message)
+        {
+            NotifyMessageStateChanged(message);
+        }
+        private void NotifyMessageStateChanged(string message) => OnMessageChange?.Invoke(message);
+    }
+}
