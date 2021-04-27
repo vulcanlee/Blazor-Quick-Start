d08c324f25ca90aba310b3738f361848520b9e8e
Vulcan <vulcan.lee@gmail.com>
2021-04-27 11:02:11 +0800

    加入註解

--- BQS15/BQS15/NotificationCenter.cs
+++ BQS15/BQS15/NotificationCenter.cs
@@ -5,8 +5,14 @@ using System.Threading.Tasks;
 
 namespace BQS15
 {
+    /// <summary>
+    /// 狀態容器 State Container ，用來觸發有變更的事件通知
+    /// </summary>
     public class NotificationCenter
     {
+        /// <summary>
+        /// 觸發事件 (Delegate 委派方法)
+        /// </summary>
         public Action<string> OnMessageChange;
 
         public void RefreshMessageAllView(string message)
@@ -14,7 +20,7 @@ namespace BQS15
             NotifyMessageStateChanged(message);
         }
         private void NotifyMessageStateChanged(string message) => OnMessageChange?.Invoke(message);
-        // 傳統舊的寫法
+        // 傳統舊的寫法 
         //private void NotifyMessageStateChangedOld(string message)
         //{
         //    if (OnMessageChange is null)
